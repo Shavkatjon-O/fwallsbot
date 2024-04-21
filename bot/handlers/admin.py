f2b98e417cfe048aa5abdbcac1de55aa3db67088
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-21 22:50:35 +0500

    feat: basic admin handler logic

---
+++ bot/handlers/admin.py
@@ -0,0 +1,14 @@
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+
+from bot.filters.admin import AdminFilter
+
+
+router = Router(name="admin")
+
+
+@router.message(Command("admin"), AdminFilter())
+async def command_admin(message: Message, state: FSMContext) -> None:
+    await message.answer("You are admin!")
