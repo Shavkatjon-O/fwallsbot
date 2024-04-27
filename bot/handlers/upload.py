9e8970d1fc091334b7a197dda325d9310c8b4f06
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-27 12:26:15 +0500

    feat: upload image handler

--- bot/handlers/upload.py
+++ bot/handlers/upload.py
@@ -28,3 +28,16 @@ async def command_upload(message: Message, state: FSMContext) -> None:
 async def back_to_manage(message: Message, state: FSMContext) -> None:
     await state.clear()
     await command_manage(message, state)
+
+
+@router.message(UploadStates.upload, F.text == UploadKeyboard.upload)
+async def upload_image(message: Message, state: FSMContext) -> None:
+    text = "📤 Пришлите мне изображение, которое хотите загрузить."
+    await message.answer(text=text, reply_markup=CancelKeyboard.get_keyboard())
+    await state.set_state(UploadStates.image)
+
+
+@router.message(UploadStates.image, F.text == CancelKeyboard.cancel)
+async def cancel_upload(message: Message, state: FSMContext) -> None:
+    await state.clear()
+    await command_upload(message, state)
