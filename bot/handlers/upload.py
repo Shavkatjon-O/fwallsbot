e8998b70e36e7072e8d687e508c9aeade25b41e5
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-27 12:05:05 +0500

    feat: add upload to manage

--- bot/handlers/upload.py
+++ bot/handlers/upload.py
@@ -1,11 +1,20 @@
 from aiogram import Router
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+
+from bot.keyboards.reply.upload import (
+    UploadKeyboard,
+    FinishKeyboard,
+    ConfirmKeyboard,
+    CancelKeyboard,
+)
+from bot.filters.admin import AdminFilter
 
 
 router = Router(name="upload")
 
 
-@router.message(Command("upload"))
-async def command_upload(message: Message) -> None:
+@router.message(Command("upload"), AdminFilter())
+async def command_upload(message: Message, state: FSMContext) -> None:
     pass
--- bot/keyboards/reply/manage.py
+++ bot/keyboards/reply/manage.py
@@ -3,15 +3,17 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 class ManageKeyboard:
-    admin = "Админы 💎"
+    admin = "💎 Админы"
+    upload = "🖼️ Обои"
 
     @classmethod
     def get_keyboard(cls) -> ReplyKeyboardMarkup:
         buttons = [
             [KeyboardButton(text=cls.admin)],
+            [KeyboardButton(text=cls.upload)],
         ]
         keyboard = ReplyKeyboardBuilder(markup=buttons)
-        keyboard.adjust(1)
+        keyboard.adjust(2)
 
         return keyboard.as_markup(
             resize_keyboard=True,
@@ -20,8 +22,10 @@ class ManageKeyboard:
 
 def get_manage_buttons() -> ReplyKeyboardMarkup:
     from bot.handlers.admin import command_admin
+    from bot.handlers.upload import command_upload
 
     buttons = {
         ManageKeyboard.admin: command_admin,
+        ManageKeyboard.upload: command_upload,
     }
     return buttons
